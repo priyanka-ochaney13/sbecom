ca346ccf4cb17ce2c5b34c61ed20f63f92e3b47d
priyanka-ochaney13 <priyanka.ochaney@gmail.com>
2026-01-06 23:20:45 +0530

    first

---
+++ src/main/java/sbecom/SbecomApplication.java
@@ -0,0 +1,13 @@
+package sbecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbecomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sbecom/SbecomApplicationTests.java
@@ -0,0 +1,13 @@
+package sbecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbecomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
